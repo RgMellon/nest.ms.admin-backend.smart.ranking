a0ec43df919d75c3cba8c72a51b8c65122fecddf
RgMellon <renan_gian@hotmail.com>
2024-09-23 06:55:21 -0300

    feat: add dateTime zone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { formatInTimeZone } from 'date-fns-tz';
 
 async function bootstrap() {
   //TODO criar anotacao substituir para createMicroService
@@ -12,6 +13,11 @@ async function bootstrap() {
       queue: 'admin-backend',
     },
   });
+
+  Date.prototype.toJSON = function (): any {
+    const timeZone = 'America/Sao_Paulo';
+    return formatInTimeZone(this, timeZone, 'yyyy-MM-dd HH:mm:ss.SSS');
+  };
 
   await app.listen();
 }
